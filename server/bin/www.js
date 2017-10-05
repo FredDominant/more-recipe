7689c34734dc93097f42935ad0fa7d36eb3f16d0
noblemajesty <Fredadewole@gmail.com>
2017-10-05 13:15:32 -0700

    Add transpile

--- server/bin/www.js
+++ server/bin/www.js
@@ -3,7 +3,7 @@ import http from 'http';
 import app from '../app';
 
 dotenv.config();
-const port = (process.env.PORT || 8080);
+const port = (process.env.PORT || 2020);
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => {
