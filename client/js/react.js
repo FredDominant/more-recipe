1bc17db8288e6b9b961b5b429a40329549200aa4
noblemajesty <Fredadewole@gmail.com>
2017-10-16 10:02:44 -0700

    Add working navbar component

--- client/js/react.js
+++ client/js/react.js
@@ -1,5 +1,9 @@
-const some = () => {
-	alert('I work from react.js');
-};
+import $ from 'jquery';
 
-export default some;
+export default $(document).ready(() => {
+  $('.dropdown-button').dropdown();
+});
+/* <form class="form-inline my-2 my-lg-0" id="search-form">
+<input class="form-control mr-sm-2 col-xs-4" id="search" type="text" placeholder="Recipe name or ingredient" aria-label="Search" />
+<button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+</form> */
