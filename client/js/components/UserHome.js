a929efb3ca6c24604ddc5771dd40e7b3692aa4eb
noblemajesty <Fredadewole@gmail.com>
2017-10-27 02:43:07 -0700

    add container class to home user home component

--- client/js/components/UserHome.js
+++ client/js/components/UserHome.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { Search } from './Search';
 import { Navbar } from './Navbar';
-import { Signup } from './Signup';
-import { Login } from './Login';
 
 export class UserHome extends React.Component {
   render() {
     return (
       <div>
-        <Navbar user="registered"/>
+				<Navbar user="registered"/>
+				<Search />
         <div className="container-fluid">
           <h3>Registered user's home page</h3>
         </div>
