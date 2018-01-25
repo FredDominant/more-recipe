995169c9833c754e477452cbb47a5dd29311018c
Fred <fredadewole@gmail.com>
2018-01-25 19:27:27 +0100

    chore(client): refactor components

--- client/js/components/ViewReviews.jsx
+++ client/js/components/ViewReviews.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ViewReviews = ({ image, firstname, lastname, createdAt, content }) => (
   <div className="container">
     <div className="row">
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-1">
+      <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1">
         <div className="reviewer-image">
           <img
             src={image}
@@ -14,10 +14,10 @@ const ViewReviews = ({ image, firstname, lastname, createdAt, content }) => (
           />
         </div>
       </div>
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-9">
+      <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10">
         <div className="content text-left mb-5">
-          <h5 className="reviewer-name"> {`${firstname} ${lastname}`} </h5>
-          <h6> {content} </h6>
+          <h5 className="reviewer-name"> <small> {`${firstname} ${lastname}`} </small> </h5>
+          <h6> <small>{content} </small></h6>
           <h6> <small id="review-date">{createdAt} </small> </h6>
         </div>
       </div>
